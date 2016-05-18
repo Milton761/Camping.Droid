efcd3d4b46bf7034c0f66e2e86b48fb19b37c977
aocsa <aocsa.cs@gmail.com>
2016-05-18 15:05:38 -0500

    ultimo commit

--- mLearningCore/MLearning.Droid/Views/Configuration.cs
+++ mLearningCore/MLearning.Droid/Views/Configuration.cs
@@ -145,25 +145,6 @@ namespace MLearning.Droid
 
 		public static string quitarErrorTildes(string Texto)
 		{
-<<<<<<< HEAD
-			if (Texto != null) {
-				//Minusculas
-				Texto =Texto.Replace ("á","á");
-				Texto =Texto.Replace ("é","é");
-				Texto =Texto.Replace ("í","í");
-				Texto =Texto.Replace ("ó","ó");
-				Texto =Texto.Replace ("ú","ú");
-				Texto =Texto.Replace ("ñ","ñ");
-				//Mayusculas
-				Texto =Texto.Replace ("Á","Á");
-				Texto =Texto.Replace ("É","É");
-				Texto =Texto.Replace ("Í","Í");
-				Texto =Texto.Replace ("Ó","Ó");
-				Texto =Texto.Replace ("Ú","Ú");
-				Texto =Texto.Replace ("Ñ","Ñ");
-			}
-
-=======
 			//Minusculas
 			if (Texto != null && Texto.Length > 0)
 			{
@@ -181,7 +162,22 @@ namespace MLearning.Droid
 				Texto = Texto.Replace("Ú", "Ú");
 				Texto = Texto.Replace ("Ñ","Ñ");
 			}
->>>>>>> origin/master
+			if (Texto != null) {
+				//Minusculas
+				Texto =Texto.Replace ("á","á");
+				Texto =Texto.Replace ("é","é");
+				Texto =Texto.Replace ("í","í");
+				Texto =Texto.Replace ("ó","ó");
+				Texto =Texto.Replace ("ú","ú");
+				Texto =Texto.Replace ("ñ","ñ");
+				//Mayusculas
+				Texto =Texto.Replace ("Á","Á");
+				Texto =Texto.Replace ("É","É");
+				Texto =Texto.Replace ("Í","Í");
+				Texto =Texto.Replace ("Ó","Ó");
+				Texto =Texto.Replace ("Ú","Ú");
+				Texto =Texto.Replace ("Ñ","Ñ");
+			}
 
 			return Texto;
 		}
